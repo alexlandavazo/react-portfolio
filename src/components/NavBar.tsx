f25dff55202f086b2cfe1083de53601f2117732e
Alejandro <alejandrolandavazo@gmail.com>
2023-01-25 21:33:13 -0700

    Change element span instead of div

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,15 +4,15 @@ const NavBar = () => {
   return (
     <div className="flex w-full h-16 text-xl font-semibold">
       <div className="flex basis-3/5 justify-between">
-        <div className="basis-56">Alejandro Landavazo</div>
-        <div className="basis-44">Front end engineer at Parsable</div>
-        <div>Based in Mexico</div>
+        <span className="basis-56">Alejandro Landavazo</span>
+        <span className="basis-44">Front end engineer</span>
+        <span>Based in Mexico</span>
       </div>
 
-      <div className="flex justify-end basis-2/5">
-        <div>Work, </div>
-        <div>About, </div>
-        <div>Contact</div>
+      <div className="flex justify-end basis-2/5 gap-2">
+        <span>Work,</span>
+        <span>About,</span>
+        <span>Contact</span>
       </div>
     </div>
   );
