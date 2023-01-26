ef56bf65ebb540c5b4b4c1a3b107d2a31a5935e0
Alejandro <alejandrolandavazo@gmail.com>
2023-01-25 20:53:48 -0700

    Refactoring code

--- src/components/AnimatedText.tsx
+++ src/components/AnimatedText.tsx
@@ -1,9 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
-
-const Wrapper = ({ children }: { children: JSX.Element }) => {
-  return <span className="word-wrapper">{children}</span>;
-};
+import { container, item } from '../utils/framer-motion';
 
 type AnimatedTextProps = {
   text: string;
@@ -14,54 +11,29 @@ const AnimatedCharacters: React.FC<AnimatedTextProps> = ({
   text,
   ...props
 }) => {
-  const [replay, setReplay] = useState(true);
-
-  const container = {
-    visible: {
-      transition: {
-        staggerChildren: 0.025,
-      },
-    },
-  };
-
-  const item = {
-    hidden: {
-      y: '200%',
-      color: '#0055FF',
-      transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 0.85 },
-    },
-    visible: {
-      y: 0,
-      color: '#004466',
-      transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 0.75 },
-      onHover: {scale: 2}
-    },
-  };
-
   const splitWords = text.split(' ');
 
-  const words: Array<any> = [];
+  const words: Array<Array<string>> = splitWords.map((word) =>
+    Array.from(word)
+  );
 
-  for (const [, item] of splitWords.entries()) {
-    words.push(item.split(''));
-  }
-
-  words.map((word) => {
-    return word.push('\u00A0');
-  });
+  (function addWhiteSpaceToWords() {
+    words.map((word) => {
+      return word.push('\u00A0');
+    });
+  })();
 
   return (
     <motion.div
       initial="hidden"
-      animate={replay ? 'visible' : 'hidden'}
+      animate={'visible'}
       variants={container}
       {...props}
     >
       {words.map((word, index) => {
         return (
-          // Wrap each word in the Wrapper component
-          <Wrapper key={index}>
-            {words[index].map((element: string, index: number) => {
+          <span key={index}>
+            {word.map((element: string, index: number) => {
               return (
                 <span
                   style={{
@@ -73,7 +45,7 @@ const AnimatedCharacters: React.FC<AnimatedTextProps> = ({
                   <motion.span
                     style={{ display: 'inline-block' }}
                     variants={item}
-                    whileHover={{scale: 1.2 }}
+                    whileHover={{ scale: 1.2 }}
                     className="m-2"
                   >
                     {element}
@@ -81,7 +53,7 @@ const AnimatedCharacters: React.FC<AnimatedTextProps> = ({
                 </span>
               );
             })}
-          </Wrapper>
+          </span>
         );
       })}
     </motion.div>
---
+++ src/utils/framer-motion.ts
@@ -0,0 +1,21 @@
+export const container = {
+  visible: {
+    transition: {
+      staggerChildren: 0.025,
+    },
+  },
+};
+
+export const item = {
+  hidden: {
+    y: '200%',
+    color: '#0055FF',
+    transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 0.85 },
+  },
+  visible: {
+    y: 0,
+    color: '#004466',
+    transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 0.75 },
+    onHover: { scale: 2 },
+  },
+};
