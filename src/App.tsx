1a39e9f411866fdd9f7c256fbeadc5caa469f309
Alejandro <alejandrolandavazo@gmail.com>
2023-01-25 21:22:05 -0700

    Create a banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import LoremIpsunPage from './components/LoremIpsunPage';
 import ScrollProgressBar from './components/ScrollProgressBar';
 import AnimatedText from './components/AnimatedText';
+import NavBar from './components/NavBar';
+
 import './App.css';
 
 function App() {
   return (
     <div>
       <ScrollProgressBar />
+      <NavBar />
       <AnimatedText text="Alex Landavazo" className="text-9xl text-center" />
       <LoremIpsunPage />
     </div>
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div className="flex w-full h-16 text-xl font-semibold">
+      <div className="flex basis-3/5 justify-between">
+        <div className="basis-56">Alejandro Landavazo</div>
+        <div className="basis-44">Front end Engineer at Parsable</div>
+        <div>Based in Mexico</div>
+      </div>
+
+      <div className="flex justify-end basis-2/5">
+        <div>Work, </div>
+        <div>About, </div>
+        <div>Contact</div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
