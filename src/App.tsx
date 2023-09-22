7ad7888cfac730d7d940e01e92043f02d9491030
Alejandro Landavazo <alejandrolandavazo@gmail.com>
2023-09-22 13:20:04 -0700

    Remove image

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import LoremIpsunPage from "./components/LoremIpsunPage";
 import ScrollProgressBar from "./components/ScrollProgressBar";
 import AnimatedText from "./components/AnimatedText";
-import AnimatedImage from "./components/AnimatedImage";
 import NavBar from "./components/NavBar";
 
 import "./App.css";
@@ -12,7 +11,6 @@ function App() {
       <ScrollProgressBar />
       <NavBar />
       <AnimatedText text="Alejandro Landavazo" className="text-9xl w-[800px]" />
-      <AnimatedImage />
     </div>
   );
 }
--- src/components/AnimatedImage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import photoUrl from '@/assets/profile-photo.jpg';
-
-const AnimatedImage: React.FC = () => {
-  return <motion.img src={photoUrl} alt="Alex Landavazo profile" />;
-};
-
-export default AnimatedImage;
